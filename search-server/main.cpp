44719b6576991d24c15c54f6b3059906a0d56cc4
NEWV0 <git2304@mail.ru>
2023-11-26 19:40:28 +0300

    answer

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main() {
     int x = 0;
     for (int i = 1; i <= 1000; ++i) {
-        if (i % 10 == 3  (i / 10) % 10 == 3  i / 100 == 3) {
+        if (i % 10 == 3 || (i / 10) % 10 == 3 || i / 100 == 3) {
             ++x;
         }
     }
