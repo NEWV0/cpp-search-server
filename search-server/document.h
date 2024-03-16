3758302fc19acd7b4eb3c8af82d3c6c5f99b052d
NEWV0 <152032646+NEWV0@users.noreply.github.com>
2024-03-16 23:40:19 +0300

    sprint 4 update 1

---
+++ search-server/document.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+#include <ostream>
+
+using namespace std;
+
+struct Document {
+    Document() = default;
+
+    Document(int id, double relevance, int rating)
+        : id(id)
+        , relevance(relevance)
+        , rating(rating) {
+    }
+
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
+};
+
+ostream& operator<<(ostream& out, const Document& document);
